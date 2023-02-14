1a6ff2f79e3c6c8df6387d313166fe167f19950e
Taeyang-Jin <wlsxodid0407@gmail.com>
2023-02-14 15:00:04 +0900

    initial

---
+++ src/main/java/com/codestates/preprojecthomework/PreProjectHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.preprojecthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectHomeworkApplication.class, args);
+	}
+
+}
